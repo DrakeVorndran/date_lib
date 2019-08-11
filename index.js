1913a1704aa2c0fbae64037a8729f1f1f2674cbd
Ki Vorndran <drake.vorndran@students.makeschool.com>
2019-08-10 18:36:00 -0700

    all functions and test written

---
+++ index.js
@@ -0,0 +1,69 @@
+class D {
+  constructor(...args) {
+    this.date = new Date(...args)
+  }
+
+  get year() {
+    return this.date.getFullYear()
+  }
+  get month() {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    return months[this.date.getMonth()]
+  }
+  get monthNum() {
+    return this.date.getMonth()
+  }
+  get day() {
+    return this.date.getDate()
+  }
+  get hour() {
+    return this.date.getHours()
+  }
+  get minute() {
+    return this.date.getMinutes()
+  }
+  get second() {
+    return this.date.getSeconds()
+  }
+  get mili() {
+    return this.date.getMilliseconds()
+  }
+
+  format(form) {
+    form = form ? form : 'Y M D'
+    const swaps = {
+      'Y': this.year,
+      'y': String(this.year).slice(2, 4),
+      'M': this.month,
+      'm': this.month.slice(0, 3),
+      'D': this.day < 10 ? '0' + this.day : this.day,
+      'd': this.day,
+      'H': this.hour < 10 ? '0' + this.hour : this.hour,
+      'h': this.hour,
+      'I': this.minute < 10 ? '0' + this.minute : this.minute,
+      'i': this.minute,
+      'S': this.second < 10 ? '0' + this.second : this.second,
+      's': this.second,
+    }
+    return form.split("").map(char => char in swaps ? swaps[char] : char).join("")
+
+  }
+
+  when(curr) {
+    curr = curr? curr : new D()
+    const l = ['mili', 'second', 'minute', 'hour', 'day', 'monthNum', 'year']
+    return l.reduce((acc, unit) => {
+      const printUnit = unit =='monthNum'? 'month' : unit
+      if (this[unit] < curr[unit]) {
+        return `${curr[unit] - this[unit]} ${printUnit}s ago`
+      }
+      else if (this[unit] > curr[unit]) {
+        return `${this[unit] - curr[unit]} ${printUnit}s from now`
+      }
+      return acc
+    }, 'now')
+  }
+
+}
+
+module.exports = D
---
+++ tests/dates.test.js
@@ -0,0 +1,39 @@
+const D = require('../index')
+
+test("Testing date creation", () => {
+  myDate = new D('1/1/2000')
+  expect(String(myDate.date)).toBe(String(new Date('1/1/2000')))
+})
+
+test("Testing time getters", () => {
+  myDate = new D(2000, 1, 1, 0, 0, 0)
+  expect(myDate.year).toBe(2000)
+  expect(myDate.month).toBe("February")
+  expect(myDate.monthNum).toBe(1)
+  expect(myDate.day).toBe(1)
+  expect(myDate.hour).toBe(0)
+  expect(myDate.minute).toBe(0)
+  expect(myDate.mili).toBe(0)
+})
+
+test("Testing .format()", () => {
+  const d = new D(2017, 0, 2, 3, 4, 5)
+  expect(d.format()).toBe('2017 January 02')
+  expect(d.format('y/m/d')).toBe('17/Jan/2')
+  expect(d.format('H:I:S')).toBe('03:04:05')
+  expect(d.format('h:i:s')).toBe('3:4:5')
+  expect(d.format('Y-M-D h:I:S')).toBe('2017-January-02 3:04:05')
+})
+
+test("Testing .when()", () => {
+  const curr = new D(2019, 6, 27, 3, 4, 5)
+
+  let d = new D(2019, 0, 2, 3, 4, 5)
+  expect(d.when(curr)).toBe('6 months ago')
+  d = new D(2019, 9, 2, 3, 4, 5)
+  expect(d.when(curr)).toBe('3 months from now')
+  d = new D(2024, 9, 2, 3, 4, 5)
+  expect(d.when(curr)).toBe('5 years from now')
+  d = new D(2019, 6, 30, 3, 4, 5)
+  expect(d.when(curr)).toBe('3 days from now')
+})
